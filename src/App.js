902171d87b0dee8e61117e957783e84d51d58245
pras75299 <singh.prashantking@gmail.com>
2021-05-02 20:01:01 +0530

    alert functionality complete

--- src/App.js
+++ src/App.js
@@ -11,12 +11,28 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if(!name){
+      // display alert
+      showAlert(true, 'danger', 'please enter value')
+    } else if(name && isEditing){
+      // work on edit functionality
+    } else{
+      // show alert
+      const newItem = {id: new Date().getTime().toString(), title: name};
+      setList(...list, newItem);
+      setName('');
+    }
+
   };
+
+  const showAlert = (show=false, type="", msg="") => {
+    setAlert({ show: show, type: type, msg: msg});
+  }
 
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
         <h3>Grocery Application</h3>
         <div className="form-control">
           <input
@@ -31,10 +47,12 @@ function App() {
           </button>
         </div>
       </form>
+      {list.length > 0 && (
       <div className="geocery-container">
-        <List />
+        <List items={list}/>
         <button className="clear-btn">Clear Items</button>
       </div>
+      )}
     </section>
   );
 }
